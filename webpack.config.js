301ffc3050aaa218d5c7ec7f9e011a2d7c71788f
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-07-14 09:18:39 +0700

    clean webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 
 module.exports = {
@@ -7,12 +8,13 @@ module.exports = {
     app: './src/index.js'
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Kedmanee'
     })
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[hash].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
