f77ab3615676473b37e0bb57dde4ea521f5d9e69
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-07-14 10:24:29 +0700

    bable loader with react preset

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var element = document.createElement('div')
+element.id = 'root'
+document.body.appendChild(element)
+
+ReactDOM.render(
+  <h1>HelloWorld!</h1>,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,21 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-
 module.exports = {
   entry: {
     app: './src/index.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },  
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
