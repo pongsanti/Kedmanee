c21cd9ff7cbe45bbed4b8a288e1707d0ad16618f
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-07-14 12:32:03 +0700

    nav bar

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import {Button, PageHeader} from 'react-bootstrap'
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem, PageHeader} from 'react-bootstrap'
 
-const HelloWorld = (props) => {
+const HelloWorld = () => {
   return (
-    <div>
-      <PageHeader>Example page header <small>Subtext for header</small></PageHeader>
-      <Button bsStyle='info'>Press me!</Button>
-    </div>
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">Kedmanee</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <NavItem eventKey={1} href="#">Link</NavItem>
+        <NavItem eventKey={2} href="#">Link</NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.4}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+      <PageHeader>Hello World</PageHeader>
+    </Navbar>
   )
 }
 
