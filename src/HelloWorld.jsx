b479102b0d615bcd359bc27464dc385b9245e099
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-07-14 10:34:29 +0700

    first react component

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Button} from 'react-bootstrap'
+
+const HelloWorld = (props) => {
+  return <Button>Press me!</Button>
+}
+
+export default HelloWorld
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import HelloWorld from './HelloWorld.jsx'
 
 var element = document.createElement('div')
 element.id = 'root'
 document.body.appendChild(element)
 
 ReactDOM.render(
-  <h1>HelloWorld!</h1>,
+  HelloWorld(),
   document.getElementById('root')
 );
